c1e0b5f57c4aae501130e020b23097e6c87dc925
Guilherme Reis <guilhermereis2009@hotmail.com>
2023-06-22 02:30:09 -0300

    Commit inicial. Projeto criado.

---
+++ android/app/src/main/kotlin/com/guilhermereis/minhasviagens/minhas_viagens/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.guilhermereis.minhasviagens.minhas_viagens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "Minhas viagens",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
