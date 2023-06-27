c40314602e7592306f5be43ddcd5f26943ebcc06
Guilherme Reis <guilhermereis2009@hotmail.com>
2023-06-27 03:21:52 -0300

    Atualiza a visualização conforme o usuário muda de posição no mapa.

--- lib/mapa.dart
+++ lib/mapa.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Mapa extends StatefulWidget {
@@ -10,23 +12,85 @@ class Mapa extends StatefulWidget {
 }
 
 class _MapaState extends State<Mapa> {
-  Completer<GoogleMapController> _controller = Completer();
-  Set<Marker> _marcadores = {};
+  final Completer<GoogleMapController> _controller = Completer();
+  final Set<Marker> _marcadores = {};
+  CameraPosition _posicaoCamera = const CameraPosition(
+    target: LatLng(-23.562436, -46.655005),
+    zoom: 18,
+  );
 
   _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
   }
 
-  _exibirMarcador(LatLng latLng) {
-    // print("Local clicado: $latLng");
-    Marker marcador = Marker(
-      markerId: MarkerId("marcador-${latLng.latitude}-${latLng.longitude}"),
-      position: latLng,
-      infoWindow: InfoWindow(title: "marcador"),
+  _exibirMarcador(LatLng latLng) async {
+    List<Placemark> listaEnderecos =
+        await placemarkFromCoordinates(latLng.latitude, latLng.longitude);
+    if (listaEnderecos.isNotEmpty) {
+      Placemark endereco = listaEnderecos[0];
+      String rua = endereco.thoroughfare.toString();
+      String estado = endereco.administrativeArea.toString();
+      String pais = endereco.country.toString();
+      String abrevPais = endereco.isoCountryCode.toString();
+      String locality = endereco.locality.toString();
+      String name = endereco.name.toString();
+      String cep = endereco.postalCode.toString();
+      String abrevRua = endereco.street.toString();
+      String cidade = endereco.subAdministrativeArea.toString();
+      String bairro = endereco.subLocality.toString();
+      String numero = endereco.subThoroughfare.toString();
+
+      Marker marcador = Marker(
+        markerId: MarkerId("marcador-${latLng.latitude}-${latLng.longitude}"),
+        position: latLng,
+        infoWindow: InfoWindow(
+          title: "$rua, $numero",
+          snippet: "Bairro $bairro, $cidade, $estado - $abrevPais",
+        ),
+      );
+      setState(() {
+        _marcadores.add(marcador);
+        print("rua: $rua"
+            "\nnumero: $numero"
+            "\nbairro: $bairro"
+            "\ncidade: $cidade"
+            "\nestado: $estado"
+            "\npais: $pais"
+            "\nabrevPais: $abrevPais"
+            "\ncep: $cep"
+            "\nlocality: $locality"
+            "\nname: $name"
+            "\nabrevRua: $abrevRua");
+      });
+    } else {
+      print("lista empty");
+    }
+  }
+
+  _movimentarCamera() async {
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(_posicaoCamera),
     );
-    setState(() {
-      _marcadores.add(marcador);
+  }
+
+  _adicionarListenerLocalizacao() {
+    Geolocator.getPositionStream(desiredAccuracy: LocationAccuracy.best)
+        .listen((Position position) {
+      setState(() {
+        _posicaoCamera = CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 18,
+        );
+        _movimentarCamera();
+      });
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _adicionarListenerLocalizacao();
   }
 
   @override
@@ -36,14 +100,12 @@ class _MapaState extends State<Mapa> {
         title: const Text("Mapa"),
       ),
       body: GoogleMap(
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(-23.562436, -46.655005),
-          zoom: 18,
-        ),
+        initialCameraPosition: _posicaoCamera,
         mapType: MapType.normal,
         onMapCreated: _onMapCreated,
         onLongPress: _exibirMarcador,
         markers: _marcadores,
+        myLocationEnabled: true,
       ),
     );
   }
